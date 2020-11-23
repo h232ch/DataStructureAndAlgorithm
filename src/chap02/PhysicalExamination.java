fd103d3dd190eaddaf70860b12f5504fed6bcee6
h232ch <dnwn7166@naver.com>
2020-11-23 17:14:12 +0900

    201103

--- src/chap02/PhysicalExamination.java
+++ src/chap02/PhysicalExamination.java
@@ -17,6 +17,7 @@ public class PhysicalExamination {
         }
     }
 
+    // 평균키
     static double aveHeight(PhyscData[] data){
         double sum = 0;
 
@@ -26,12 +27,13 @@ public class PhysicalExamination {
         return sum / data.length;
     }
 
+    //
     static void distVision(PhyscData[] data, int[] dist){
         int i = 0;
         dist[i] = 0;
         for(i = 0; i < data.length; i++){
             if(data[i].vision >= 0.0 && data[i].vision <= VMAX / 10.0)
-            dist[(int)(data[i].vision * 10)]++;
+                dist[(int)(data[i].vision * 10)]++;
         }
     }
 
@@ -41,7 +43,7 @@ public class PhysicalExamination {
                 new PhyscData("박현규", 172, 0.3),
                 new PhyscData("최윤아", 162, 0.7),
                 new PhyscData("최윤미", 158, 1.2),
-                new PhyscData("홍연의", 160, 0.5),
+                new PhyscData("홍연의", 160, 0.4),
                 new PhyscData("김영준", 180, 0.4),
                 new PhyscData("박용규", 170, 1.5),
         };
@@ -58,10 +60,16 @@ public class PhysicalExamination {
         distVision(x, vdist);
 
         System.out.println("\n시력 분포");
-        for(int i = 0; i < VMAX; i++){
-            System.out.printf("%3.1f~:%2d명\n", i / 10.0, vdist[i]);
+//        for(int i = 0; i < VMAX; i++){
+//            System.out.printf("%3.1f~:%2d명\n", i / 10.0, vdist[i]);
+//        }
+
+        for(int i = 0; i < VMAX; i++){ // Q10 : 시력분포를 *으로 나타내시오
+            System.out.printf("%3.1f~: ", i / 10.0);
+            for(int j = 0; j < vdist[i]; j++){
+                System.out.print("*");
+            }
+            System.out.printf("\n");
         }
     }
-
-
 }
