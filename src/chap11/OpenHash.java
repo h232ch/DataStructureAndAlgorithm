bb869b2d9df1ed571326f84b8a7243a3fc355eb8
dnwn7166 <dnwn7166@gmail.com>
2021-01-31 21:31:35 +0900

    210131

--- src/chap11/OpenHash.java
+++ src/chap11/OpenHash.java
@@ -6,17 +6,17 @@ public class OpenHash<K, V> {
     enum Status {OCCUPIED, EMPTY, DELETED}; // 데이터 저장, 비어있음, 삭제마침
 
     // 버킷
-    static class Buecket<K, V> {
+    static class Bucket<K, V> {
         private K key;
         private V data;
         private Status stat;
 
-        Buecket() {
+        Bucket() {
             stat = Status.EMPTY;
         }
 
         // 모든 필드에 값을 설정합니다.
-        void Set(K key, V data, Status stat) {
+        void set(K key, V data, Status stat) {
             this.key = key;
             this.data = data;
             this.stat = stat;
@@ -43,14 +43,14 @@ public class OpenHash<K, V> {
     }
 
     private int size; // 해시 테이블의 크기
-    private Buecket<K,V>[] table; // 해시 테이블
+    private Bucket<K,V>[] table; // 해시 테이블
 
     // 생성자
     public OpenHash(int size) {
         try {
-            table = new Buecket[size];
+            table = new Bucket[size];
             for (int i = 0; i < size; i++) {
-                table[i] = new Buecket<>();
+                table[i] = new Bucket<>();
             }
             this.size = size;
         } catch (OutOfMemoryError error) { // 해시 테이블을 생성할 수 없음
@@ -62,5 +62,85 @@ public class OpenHash<K, V> {
     // 해시 값을 구함
     public int hashValue(Object key) {
         return key.hashCode() % size;
+    }
+
+    // 재해시 값을 구함
+    public int rehashValue(int hash) {
+        return (hash + 1) % size;
+    }
+
+    // 키 값 key를 갖는 버킷의 검색
+    private Bucket<K, V> searchNode(K key) {
+        int hash = hashValue(key); // 검색할 데이터의 해시값
+        Bucket<K,V> p = table[hash]; // 선택 버킷
+
+        for (int i = 0; p.stat != Status.EMPTY && i < size; i++) {
+            if (p.stat == Status.OCCUPIED && p.getKey().equals(key)) {
+                return p;
+            }
+            hash = rehashValue(hash); // 재해시
+            p = table[hash];
+        }
+        return null;
+    }
+
+    // 키 값 key를 갖는 요소의 검색 (데이터를 반환)
+    public V search(K key) {
+        Bucket<K, V> p = searchNode(key);
+        if (p != null) {
+            return p.getValue();
+        } else {
+            return null;
+        }
+    }
+
+    // 키 값 key, 데이터 data를 갖는 요소의 추가
+    public int add(K key, V data) {
+        if (search(key) != null) {
+            return 1;
+        }
+
+        int hash = hashValue(key);
+        Bucket<K, V> p = table[hash];
+        for (int i = 0; i < size; i++) {
+            if (p.stat == Status.EMPTY || p.stat == Status.DELETED) {
+                p.set(key, data, Status.OCCUPIED);
+                return 0;
+            }
+            hash = rehashValue(hash);
+            p = table[hash];
+        }
+        return 2;
+    }
+
+    // 키 값 key를 갖는 요소의 삭제
+    public int remove(K key) {
+        Bucket<K, V> p = searchNode(key);
+        if (p == null) {
+            return 1;
+        }
+        p.setStat(Status.DELETED);
+        return 0;
+    }
+
+    // 해시 테이블을 덤프
+    public void dump() {
+        for (int i = 0; i < size; i++) {
+            System.out.printf("%02d ", i);
+            switch (table[i].stat) {
+                case OCCUPIED:
+                    System.out.printf("%s (%s)\n",
+                            table[i].getKey(), table[i].getValue());
+                    break;
+
+                case EMPTY:
+                    System.out.println("-- 미등록 --");
+                    break;
+
+                case DELETED:
+                    System.out.println("-- 삭제마침 --");
+                    break;
+            }
+        }
     }
 }
